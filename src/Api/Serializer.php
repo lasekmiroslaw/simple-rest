96d0b0e700cf132e7de7bc58a078448fc59570bb
Miro <lasekmiroslaw@gmail.com>
2018-04-11 15:39:40 +0200

    add serializer

---
+++ src/Api/Serializer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Api;
+
+use JMS\Serializer\SerializerBuilder;
+
+class Serializer
+{
+    public function serialize($data, $form = 'json')
+    {
+      $serializer = \JMS\Serializer\SerializerBuilder::create()
+          ->build();
+
+        return $serializer->serialize($data, $form);
+    }
+}
